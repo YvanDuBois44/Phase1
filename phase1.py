7622fbb4f021c2ec736bc4129752c2a966869b75
Olivier <OLROY58@ulaval.ca>
2023-11-13 23:15:43 -0500

    Correction

--- phase1.py
+++ phase1.py
@@ -1,20 +1,24 @@
+#Valeur de titres boursier 
 import argparse
-import requests
 import json
 import datetime
+import requests
 
-
+#Analyse de la ligne de commande
 def analyser_commande():
-    parser = argparse.ArgumentParser(description= 'Extraction de valeurs historiques pour un ou plusieurs symboles boursiers')
-    parser.add_argument('-d', '--début', help="Date recherché la plus ancienne (format: AAAA-MM-JJ)", metavar='DATE', type=datetime.date.fromisoformat)
-    parser.add_argument('-f', '--fin', help="Date recherché la plus récente (format: AAAA-MM-JJ)", metavar='DATE', type=datetime.date.fromisoformat)
-    parser.add_argument('-v', '--valeur', help="La valeur désirée (par default: fermeture)", choices=['fermeture','ouverture','min','max','volume'])
+    parser = argparse.ArgumentParser(description=
+                                     'Extraction de valeurs historiques pour un ou plusieurs symboles boursiers')
+    parser.add_argument('-d', '--début', help="Date recherché la plus ancienne (format: AAAA-MM-JJ)", metavar='DATE',
+                        type=datetime.date.fromisoformat)
+    parser.add_argument('-f', '--fin', help="Date recherché la plus récente (format: AAAA-MM-JJ)", metavar='DATE',
+                        type=datetime.date.fromisoformat)
+    parser.add_argument('-v', '--valeur', help="La valeur désirée (par default: fermeture)",
+                        choices=['fermeture','ouverture','min','max','volume'])
     parser.add_argument('symbole', nargs='+', help="Nom d'un symbole boursier")
     return parser.parse_args()
 
-
+#Production de l'historique de la bourse
 def produire_historique(symbole, debut, fin, valeur):
-
     if fin is None:
         fin = datetime.date.today()
     if debut is None:
@@ -22,19 +26,17 @@ def produire_historique(symbole, debut, fin, valeur):
     if valeur is None:
         valeur = 'fermeture'
 
-    for i in symbole:
-        
-        url = f'https://pax.ulaval.ca/action/{i}/historique/'
+    for compagnie in symbole:
+        url = f'https://pax.ulaval.ca/action/{compagnie}/historique/'
         params = {'début': debut, 'fin': fin}
         réponse = requests.get(url=url, params=params)
         réponse = json.loads(réponse.text)
-
         liste = []
         for j in réponse['historique']:
-            liste.append(((datetime.datetime.strptime(j, '%Y-%m-%d')).date(), réponse['historique'][j][valeur]))
+            liste.append(((datetime.datetime.strptime(j, '%Y-%m-%d')).date(), 
+                          réponse['historique'][j][valeur]))
 
-    print(f'titre={i}: valeur={valeur}, début={repr(debut)}, fin={repr(fin)}', liste)
+    print(f'titre={compagnie}: valeur={valeur}, début={repr(debut)}, fin={repr(fin)}', liste)
 
-
-produire_historique(analyser_commande().symbole, analyser_commande().début, analyser_commande().fin, analyser_commande().valeur)
-
+args = analyser_commande()
+produire_historique(args.symbole, args.début, args.fin, args.valeur)
