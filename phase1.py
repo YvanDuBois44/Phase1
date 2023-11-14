03e1fa21eac79335bdf3fddc24ddc41d7b251e27
Olivier <OLROY58@ulaval.ca>
2023-11-13 19:30:11 -0500

    Oui

--- phase1.py
+++ phase1.py
@@ -4,6 +4,12 @@ import argparse
 parser = argparse.ArgumentParser(description= 'Extraction de valeurs historiques pour un ou plusieurs symboles boursiers')
 
 
+parser.add_argument('-d', '--début',help="Date recherché la plus ancienne (format: AAAA-MM-JJ)", metavar='DATE')
+parser.add_argument('-f', '--fin',help="Date recherché la plus récente (format: AAAA-MM-JJ)", metavar='DATE')
+parser.add_argument('-v', '--valeur',help="La valeur désirée (par default: fermeture)", metavar={'fermeture', 'ouverture', 'min', 'max', 'volume'})
+
+
+'''
 parser.add_argument(
     '-m', '--mode',
     metavar='TYPE',
@@ -12,6 +18,7 @@ parser.add_argument(
     choices=['type1', 'type2', 'type3', 'type4'],
     help="Une description de ce que représente cet argument",
 )
+'''
 
 args = parser.parse_args()
 
