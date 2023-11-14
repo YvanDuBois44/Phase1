464028ed9bf66d06f0c070fc760138dd0d5a173c
Olivier <OLROY58@ulaval.ca>
2023-11-13 20:14:15 -0500

    Analyser_commande Terminer

--- phase1.py
+++ phase1.py
@@ -1,24 +1,15 @@
 import argparse
 
 
-parser = argparse.ArgumentParser(description= 'Extraction de valeurs historiques pour un ou plusieurs symboles boursiers')
+
+def analyser_commande():
+    parser = argparse.ArgumentParser(description= 'Extraction de valeurs historiques pour un ou plusieurs symboles boursiers')
+    parser.add_argument('-d', '--début',help="Date recherché la plus ancienne (format: AAAA-MM-JJ)", metavar='DATE')
+    parser.add_argument('-f', '--fin',help="Date recherché la plus récente (format: AAAA-MM-JJ)", metavar='DATE')
+    parser.add_argument('-v', '--valeur',help="La valeur désirée (par default: fermeture)", choices=['fermeture','ouverture','min','max','volume'])
+    parser.add_argument('symbole', nargs='+', help="Nom d'un symbole boursier")
+    return parser.parse_args()
 
 
-parser.add_argument('-d', '--début',help="Date recherché la plus ancienne (format: AAAA-MM-JJ)", metavar='DATE')
-parser.add_argument('-f', '--fin',help="Date recherché la plus récente (format: AAAA-MM-JJ)", metavar='DATE')
-parser.add_argument('-v', '--valeur',help="La valeur désirée (par default: fermeture)", metavar={'fermeture', 'ouverture', 'min', 'max', 'volume'})
-
-
-'''
-parser.add_argument(
-    '-m', '--mode',
-    metavar='TYPE',
-    dest='mode',
-    default='type1',
-    choices=['type1', 'type2', 'type3', 'type4'],
-    help="Une description de ce que représente cet argument",
-)
-'''
-
-args = parser.parse_args()
-
+def produire_historique(symbole, debut, fin, valeur)
+analyser_commande()
