a46292673e9c02d9ef979b69f5702b1cc70be057
Olivier <OLROY58@ulaval.ca>
2023-11-13 22:20:01 -0500

    Main terminer reste exception

--- phase1.py
+++ phase1.py
@@ -12,16 +12,20 @@ def analyser_commande():
     parser.add_argument('symbole', nargs='+', help="Nom d'un symbole boursier")
     return parser.parse_args()
 
-print(type(analyser_commande().début))
 
 def produire_historique(symbole, debut, fin, valeur):
+    
     for i in symbole:
         url = f'https://pax.ulaval.ca/action/{i}/historique/'
         params = {'début': debut, 'fin': fin}
         réponse = requests.get(url=url, params=params)
         réponse = json.loads(réponse.text)
 
-        print(f'titre={i}: valeur={valeur}, début={debut}, fin={fin}')
+        liste = []
+        for j in réponse['historique']:
+            liste.append(((datetime.datetime.strptime(j, '%Y-%m-%d')).date(), réponse['historique'][j][valeur]))
+
+    print(f'titre={i}: valeur={valeur}, début={repr(debut)}, fin={repr(fin)}', liste)
 
 
 produire_historique(analyser_commande().symbole, analyser_commande().début, analyser_commande().fin, analyser_commande().valeur)
