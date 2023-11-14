d58f6f52317920d385cfa5caf70d9b09fd6fe87d
Olivier <OLROY58@ulaval.ca>
2023-11-13 19:15:31 -0500

    yo

--- phase1.py
+++ phase1.py
@@ -1,4 +1,17 @@
-import argpasrse
+import argparse
 
 
 parser = argparse.ArgumentParser(description= 'Extraction de valeurs historiques pour un ou plusieurs symboles boursiers')
+
+
+parser.add_argument(
+    '-m', '--mode',
+    metavar='TYPE',
+    dest='mode',
+    default='type1',
+    choices=['type1', 'type2', 'type3', 'type4'],
+    help="Une description de ce que représente cet argument",
+)
+
+args = parser.parse_args()
+
